5b9a3b00f5155c117bf7e191dc34cd693362f7c3
Stanislav Tebloev <stas.tebloev@yandex.com>
2020-03-17 14:54:04 +0300

    Modularize solution

--- java-solutions/ru/ifmo/rain/tebloev/implementor/build-jar.sh
+++ java-solutions/ru/ifmo/rain/tebloev/implementor/build-jar.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+tmppackageroot="_build/ru.ifmo.rain.tebloev.implementor/ru/ifmo/rain/tebloev/implementor"
+mkdir -p $tmppackageroot
+
+cp ./*.java $tmppackageroot
+mv $tmppackageroot/module-info.java _build/ru.ifmo.rain.tebloev.implementor
+
 cd ../../../../../../../
 
-output="./java-advanced-2020-solutions/java-solutions/ru/ifmo/rain/tebloev/implementor/_build"
-classpath="./java-advanced-2020/modules/info.kgeorgiy.java.advanced.implementor"
-sources="./java-advanced-2020-solutions/java-solutions/ru/ifmo/rain/tebloev/implementor/*.java"
+packageroot="./java-advanced-2020-solutions/java-solutions/ru/ifmo/rain/tebloev/implementor"
+output="$packageroot/_build/out"
+modulepath="./java-advanced-2020/lib/:./java-advanced-2020/artifacts/"
+modulesourcepath="$packageroot/_build"
+javac --module ru.ifmo.rain.tebloev.implementor --module-source-path $modulesourcepath -p $modulepath -d $output
 
-javac -d $output -cp "$classpath" $sources
-
-cd ./java-advanced-2020-solutions/java-solutions/ru/ifmo/rain/tebloev/implementor/_build || exit
-jar --create --file ../_implementor.jar ./ru
+cd $packageroot || exit 1
+jar --create --file=_implementor.jar --module-version=1.0 -C _build/out/ru.ifmo.rain.tebloev.implementor .
---
+++ java-solutions/ru/ifmo/rain/tebloev/implementor/module-info.java
@@ -0,0 +1,5 @@
+module ru.ifmo.rain.tebloev.implementor {
+    exports ru.ifmo.rain.tebloev.implementor;
+    requires info.kgeorgiy.java.advanced.implementor;
+    requires java.compiler;
+}
