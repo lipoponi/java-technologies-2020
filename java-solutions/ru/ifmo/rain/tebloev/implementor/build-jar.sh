b252b91dc82ffbfdedc8f72ffd11064acceb8407
Stanislav Tebloev <stas.tebloev@yandex.com>
2020-03-23 21:48:59 +0300

    Change dirname to correct one

--- java-solutions/ru/ifmo/rain/tebloev/implementor/build-jar.sh
+++ java-solutions/ru/ifmo/rain/tebloev/implementor/build-jar.sh
@@ -8,10 +8,10 @@ rm -rf "$workingdir/_build"
 cd ../../../../../../
 modulepath="../java-advanced-2020/lib/:../java-advanced-2020/artifacts/"
 output="$workingdir/_build"
-javac --module java.solutions --module-source-path "." -p "$modulepath" -d "$output"
+javac -p "$modulepath" -d "$output" $(find -name "*.java")
 
 cd "$workingdir" || exit 1
 jar --create --file=_implementor.jar \
   --module-version=1.0 \
   --main-class=ru.ifmo.rain.tebloev.implementor.JarImplementor \
-  -C _build/java.solutions .
+  -C _build/ module-info.class
