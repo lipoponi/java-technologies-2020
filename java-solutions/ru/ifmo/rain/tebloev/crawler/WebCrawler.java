366922258968aae1bbc0d62da950815f8a3741cb
Stanislav Tebloev <stas.tebloev@yandex.com>
2020-05-02 13:00:56 +0300

    Refactor

--- java-solutions/ru/ifmo/rain/tebloev/crawler/WebCrawler.java
+++ java-solutions/ru/ifmo/rain/tebloev/crawler/WebCrawler.java
@@ -15,14 +15,6 @@ public class WebCrawler implements Crawler {
     private final int perHost;
     private final ConcurrentMap<String, Host> hostMap;
 
-    public WebCrawler(Downloader downloader, int downloaderCount, int extractorCount, int perHost) {
-        this.downloader = downloader;
-        this.downloadExecutor = Executors.newFixedThreadPool(downloaderCount);
-        this.extractExecutor = Executors.newFixedThreadPool(extractorCount);
-        this.perHost = perHost;
-        this.hostMap = new ConcurrentHashMap<>();
-    }
-
     private class Host {
         private final BlockingQueue<Runnable> waiting = new LinkedBlockingQueue<>();
         public final Semaphore semaphore = new Semaphore(perHost);
@@ -50,106 +42,6 @@ public class WebCrawler implements Crawler {
         }
     }
 
-    private static <V extends Exception> V mergeWithSuppression(V a, V b) {
-        a.addSuppressed(b);
-        return a;
-    }
-
-    private List<String> processLayer(List<String> layer, List<String> downloaded, ConcurrentMap<String, IOException> errors, Set<String> seen, boolean extractLinks) {
-        CountDownLatch downloadLatch = new CountDownLatch(layer.size());
-        Queue<Future<List<String>>> extractFutures = new ConcurrentLinkedQueue<>();
-
-        layer.forEach(url -> {
-            try {
-                Host host = hostMap.computeIfAbsent(URLUtils.getHost(url), (key) -> new Host());
-
-                host.addJob(() -> {
-                    try {
-                        Document document = this.downloader.download(url);
-                        downloaded.add(url);
-
-                        if (extractLinks) {
-                            extractFutures.add(extractExecutor.submit(() -> {
-                                try {
-                                    return document.extractLinks();
-                                } catch (IOException e) {
-                                    errors.merge(url, e, WebCrawler::mergeWithSuppression);
-                                }
-
-                                return List.of();
-                            }));
-                        }
-                    } catch (IOException e) {
-                        errors.merge(url, e, WebCrawler::mergeWithSuppression);
-                    } finally {
-                        downloadLatch.countDown();
-                        host.semaphore.release();
-                        host.startOneWaiting();
-                    }
-                });
-            } catch (MalformedURLException e) {
-                errors.merge(url, e, WebCrawler::mergeWithSuppression);
-            }
-        });
-
-        try {
-            downloadLatch.await();
-            List<List<String>> extractResults = new ArrayList<>();
-            for (Future<List<String>> future : extractFutures) {
-                extractResults.add(future.get());
-            }
-            return extractResults.stream().flatMap(Collection::stream).distinct()
-                    .filter(url -> !seen.contains(url)).peek(seen::add)
-                    .collect(Collectors.toList());
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        } catch (ExecutionException ignored) {
-            assert false : "Unreachable";
-        }
-
-        return List.of();
-    }
-
-    @Override
-    public Result download(String startUrl, int depth) {
-        if (downloadExecutor.isShutdown() || extractExecutor.isShutdown()) {
-            throw new UnsupportedOperationException("WebCrawler is shutdown");
-        }
-
-        List<String> downloaded = Collections.synchronizedList(new ArrayList<>());
-        ConcurrentMap<String, IOException> errors = new ConcurrentHashMap<>();
-        Set<String> seen = new HashSet<>();
-        List<String> activeLayer = List.of(startUrl);
-
-        seen.add(startUrl);
-
-        for (int i = 1; i <= depth && !activeLayer.isEmpty(); i++) {
-            activeLayer = processLayer(activeLayer, downloaded, errors, seen, i < depth);
-        }
-
-        return new Result(downloaded, errors);
-    }
-
-    private void shutdownAndAwaitTermination(ExecutorService executor) {
-        executor.shutdown();
-        try {
-            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
-                executor.shutdownNow();
-                if (!executor.awaitTermination(60, TimeUnit.SECONDS))
-                    System.err.println("Pool did not terminate");
-            }
-        } catch (InterruptedException ie) {
-            executor.shutdownNow();
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    @Override
-    public void close() {
-        shutdownAndAwaitTermination(downloadExecutor);
-        shutdownAndAwaitTermination(extractExecutor);
-    }
-
     private static class Validator {
         public static void notNull(String value) {
             if (value == null) {
@@ -166,6 +58,11 @@ public class WebCrawler implements Crawler {
                 throw new IllegalArgumentException("Invalid number format", e);
             }
         }
+    }
+
+    private static <V extends Exception> V mergeWithSuppression(V a, V b) {
+        a.addSuppressed(b);
+        return a;
     }
 
     private static void printError(String msg) {
@@ -220,5 +117,111 @@ public class WebCrawler implements Crawler {
         } catch (IllegalArgumentException e) {
             printError("Usage: WebCrawler url [depth [downloads [extractors [perHost]]]]");
         }
+    }
+
+    public WebCrawler(Downloader downloader, int downloaderCount, int extractorCount, int perHost) {
+        this.downloader = downloader;
+        this.downloadExecutor = Executors.newFixedThreadPool(downloaderCount);
+        this.extractExecutor = Executors.newFixedThreadPool(extractorCount);
+        this.perHost = perHost;
+        this.hostMap = new ConcurrentHashMap<>();
+    }
+
+
+    private List<String> processLayer(List<String> layer, List<String> downloaded, ConcurrentMap<String, IOException> errors, Set<String> seen, boolean extractLinks) {
+        CountDownLatch downloadLatch = new CountDownLatch(layer.size());
+        Queue<Future<List<String>>> extractFutures = new ConcurrentLinkedQueue<>();
+
+        layer.forEach(url -> {
+            try {
+                Host host = hostMap.computeIfAbsent(URLUtils.getHost(url), (key) -> new Host());
+
+                host.addJob(() -> {
+                    try {
+                        Document document = this.downloader.download(url);
+                        downloaded.add(url);
+
+                        if (extractLinks) {
+                            extractFutures.add(extractExecutor.submit(() -> {
+                                try {
+                                    return document.extractLinks();
+                                } catch (IOException e) {
+                                    errors.merge(url, e, WebCrawler::mergeWithSuppression);
+                                }
+
+                                return List.of();
+                            }));
+                        }
+                    } catch (IOException e) {
+                        errors.merge(url, e, WebCrawler::mergeWithSuppression);
+                    } finally {
+                        downloadLatch.countDown();
+                        host.semaphore.release();
+                        host.startOneWaiting();
+                    }
+                });
+            } catch (MalformedURLException e) {
+                errors.merge(url, e, WebCrawler::mergeWithSuppression);
+            }
+        });
+
+        try {
+            downloadLatch.await();
+
+            List<List<String>> extractResults = new ArrayList<>();
+            for (Future<List<String>> future : extractFutures) {
+                extractResults.add(future.get());
+            }
+
+            return extractResults.stream().flatMap(Collection::stream).distinct()
+                    .filter(url -> !seen.contains(url)).peek(seen::add)
+                    .collect(Collectors.toList());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException ignored) {
+            assert false : "Unreachable";
+        }
+
+        return List.of();
+    }
+
+    @Override
+    public Result download(String startUrl, int depth) {
+        if (downloadExecutor.isShutdown() || extractExecutor.isShutdown()) {
+            throw new UnsupportedOperationException("WebCrawler is shutdown");
+        }
+
+        List<String> downloaded = Collections.synchronizedList(new ArrayList<>());
+        ConcurrentMap<String, IOException> errors = new ConcurrentHashMap<>();
+        Set<String> seen = new HashSet<>();
+        List<String> activeLayer = List.of(startUrl);
+
+        seen.add(startUrl);
+
+        for (int i = 1; i <= depth && !activeLayer.isEmpty(); i++) {
+            activeLayer = processLayer(activeLayer, downloaded, errors, seen, i < depth);
+        }
+
+        return new Result(downloaded, errors);
+    }
+
+    private void shutdownAndAwaitTermination(ExecutorService executor) {
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+                if (!executor.awaitTermination(60, TimeUnit.SECONDS))
+                    System.err.println("Pool did not terminate");
+            }
+        } catch (InterruptedException ie) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @Override
+    public void close() {
+        shutdownAndAwaitTermination(downloadExecutor);
+        shutdownAndAwaitTermination(extractExecutor);
     }
 }
