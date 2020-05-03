0825090087dd9b5c593e482d55c9f65184e2fd37
Stanislav Tebloev <stas.tebloev@yandex.com>
2020-05-03 23:17:40 +0300

    Refactor client

--- java-solutions/ru/ifmo/rain/tebloev/hello/HelloUDPClient.java
+++ java-solutions/ru/ifmo/rain/tebloev/hello/HelloUDPClient.java
@@ -7,13 +7,15 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
-import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
 public class HelloUDPClient implements HelloClient {
+    private static final int SOCKET_TIMEOUT_MS = 100;
+    private static final int ATTEMPTS_PER_REQUEST = 100;
+
     @Override
     public void run(String host, int port, String prefix, int threads, int requests) {
         SocketAddress serverAddress = new InetSocketAddress(host, port);
@@ -23,28 +25,26 @@ public class HelloUDPClient implements HelloClient {
 
         IntStream.range(0, threads).forEach(threadId -> senderExecutor.execute(() -> {
             try (DatagramSocket socket = new DatagramSocket(null)) {
-                socket.setSoTimeout(100);
+                socket.setSoTimeout(SOCKET_TIMEOUT_MS);
                 socket.connect(serverAddress);
 
-                byte[] buffer = new byte[1024];
-                for (int i = 0; i < requests; i++) {
-                    String request = String.format("%s%d_%d", prefix, threadId, i);
+                for (int requestId = 0; requestId < requests; requestId++) {
+                    String request = String.format("%s%d_%d", prefix, threadId, requestId);
+                    DatagramPacket sendPacket = Util.createDefaultSendPacket(serverAddress, request);
 
-                    byte[] queryBytes = request.getBytes(StandardCharsets.UTF_8);
-                    socket.send(new DatagramPacket(queryBytes, queryBytes.length, serverAddress));
+                    for (int j = 0; j < ATTEMPTS_PER_REQUEST; j++) {
+                        try {
+                            socket.send(sendPacket);
+                            DatagramPacket receivePacket = Util.createDefaultReceivePacket();
+                            socket.receive(receivePacket);
+                            String response = Util.extractString(receivePacket);
 
-                    try {
-                        DatagramPacket responsePacket = new DatagramPacket(buffer, 1024);
-                        socket.receive(responsePacket);
-
-                        String response = new String(responsePacket.getData(), responsePacket.getOffset(), responsePacket.getLength(), StandardCharsets.UTF_8);
-                        if (response.contains(request)) {
-                            System.out.println(String.format("%s%n%s", request, response));
-                        } else {
-                            i--;
+                            if (response.contains(request)) {
+                                System.out.println(String.format("%s%n%s", request, response));
+                                break;
+                            }
+                        } catch (IOException ignored) {
                         }
-                    } catch (IOException e) {
-                        i--;
                     }
                 }
             } catch (IOException ignored) {
@@ -55,9 +55,10 @@ public class HelloUDPClient implements HelloClient {
 
         try {
             senderLatch.await();
-        } catch (
-                InterruptedException e) {
+        } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+        } finally {
+            Util.shutdownAndAwaitTermination(senderExecutor);
         }
     }
 }
