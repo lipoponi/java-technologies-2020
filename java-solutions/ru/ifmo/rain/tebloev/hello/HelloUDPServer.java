391c17f51602a942b4ba9691c9a6dad3f95927f3
Stanislav Tebloev <stas.tebloev@yandex.com>
2020-05-09 21:25:37 +0300

    Change exception handling and make `close` method wait for threads to complete

--- java-solutions/ru/ifmo/rain/tebloev/hello/HelloUDPServer.java
+++ java-solutions/ru/ifmo/rain/tebloev/hello/HelloUDPServer.java
@@ -3,24 +3,40 @@ package ru.ifmo.rain.tebloev.hello;
 import info.kgeorgiy.java.advanced.hello.HelloServer;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.IntStream;
 
 public class HelloUDPServer implements HelloServer {
-    private ExecutorService receiveExecutor = null;
     private DatagramSocket socket = null;
+    private ExecutorService receiveExecutor = null;
+    private CountDownLatch executorLatch = null;
 
+    /**
+     * Starts a new Hello server.
+     *
+     * @param port    server port
+     * @param threads number of working threads
+     * @throws UncheckedIOException if socket cannot be allocated
+     */
     @Override
     public synchronized void start(int port, int threads) {
         try {
-            receiveExecutor = Executors.newFixedThreadPool(threads);
             socket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            throw new UncheckedIOException("Socket cannot be allocated", e);
+        }
 
-            IntStream.range(0, threads).forEach(threadId -> receiveExecutor.execute(() -> {
+        receiveExecutor = Executors.newFixedThreadPool(threads);
+        executorLatch = new CountDownLatch(threads);
+
+        IntStream.range(0, threads).forEach(threadId -> receiveExecutor.execute(() -> {
+            try {
                 while (!Thread.currentThread().isInterrupted() && !socket.isClosed()) {
                     try {
                         DatagramPacket receivePacket = Util.createDefaultReceivePacket();
@@ -32,14 +48,20 @@ public class HelloUDPServer implements HelloServer {
                     } catch (IOException ignored) {
                     }
                 }
-            }));
-        } catch (SocketException ignored) {
-        }
+            } finally {
+                executorLatch.countDown();
+            }
+        }));
     }
 
     @Override
     public synchronized void close() {
         socket.close();
         Util.shutdownAndAwaitTermination(receiveExecutor);
+
+        try {
+            executorLatch.await();
+        } catch (InterruptedException ignored) {
+        }
     }
 }
