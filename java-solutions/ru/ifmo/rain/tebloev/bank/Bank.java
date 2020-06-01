e9e917b27b9c514ad884bb0f55b50b2ad51fed1a
Stanislav Tebloev <stas.tebloev@yandex.com>
2020-06-02 01:02:33 +0300

    Modify Bank interface

--- java-solutions/ru/ifmo/rain/tebloev/bank/Bank.java
+++ java-solutions/ru/ifmo/rain/tebloev/bank/Bank.java
@@ -19,4 +19,23 @@ public interface Bank extends Remote {
      * @return account with specified identifier or {@code null} if such account does not exists.
      */
     Account getAccount(String id) throws RemoteException;
+
+    /**
+     * Creates a new person with specified data or returns existing one.
+     *
+     * @param firstName First name of person
+     * @param lastName  Last name of person
+     * @param passport  Unique identification number
+     * @return created or existing person
+     */
+    Person createPerson(String firstName, String lastName, String passport) throws RemoteException;
+
+    /**
+     * Returns person by passport.
+     *
+     * @param passport Unique identification number
+     * @param remote   {@code true} if instance should be remote, {@code false} if local
+     * @return person with specified passport or {@code null} if person with that passport doesn't exists
+     */
+    Person getPerson(String passport, boolean remote) throws RemoteException;
 }
